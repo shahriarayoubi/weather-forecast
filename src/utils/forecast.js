1df987533e049c605fe0ec779010d69741d11a13
Shahriar Ayoubi <shahriarayoubi@gmail.com>
2019-03-15 22:48:47 +0000

    Added high and low temperatures

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback(body.error, undefined);
         } else {
-        callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of rain.`);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh}, with a low of ${body.daily.data[0].temperatureLow}. There is ${body.currently.precipProbability}% chance of rain.`);
         }
     })
 };
