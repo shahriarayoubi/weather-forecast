ff8077ce59c4eb0ecc853cd5d43a7c9d6a2ad5f5
Shahriar Ayoubi <shahriarayoubi@gmail.com>
2019-03-15 22:19:41 +0000

    Make changes for Heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value;
     console.log(location);
 
-    fetch(`http://localhost:5000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error;
